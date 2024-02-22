cc53406f470f179bdf0fe13f161f305356628be6
shrinivas <shinivassb16@gmail.com>
2024-02-22 19:08:41 +0530

    updated

---
+++ backend.sh
@@ -0,0 +1,39 @@
+echo -e "\e[34m Disabling Nodejs default version \e[0m"
+dnf module disable nodejs -y
+
+echo -e "\e[34m Enabling nodejs 18 version \e[0m"
+dnf module enable nodejs:18 -y
+
+echo -e "\e[34m Installing nodejs \e[0m"
+dnf install nodejs - y
+
+echo -e "\e[34m Copying backend config file \e[0m"
+cp backend.service /etc/systemd/system/backend.service
+
+echo -e "\e[34m Adding expense user \e[0m"
+useradd expense
+
+echo -e "\e[34m Creating directory \e[0m"
+mkdir /app
+
+echo -e "\e[34m Downloading and extracting backend file \e[0m"
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip 
+cd /app 
+unzip /tmp/backend.zip
+
+echo -e "\e[34m Installing nodejs dependencies \e[0m"
+cd /app 
+npm install
+
+echo -e "\e[34m Installing mysql client \e[0m"
+dnf install mysql -y
+
+echo -e "\e[34m Loading the schema \e[0m"
+mysql -h mysql-dev.sbadiger93.online -uroot -pExpenseApp@1 < /app/schema/backend.sql 
+
+
+echo -e "\e[34m Restarting backend \e[0m"
+systemctl daemon-reload
+systemctl enable backend
+systemctl restart backend
+
---
+++ mysql.sh
@@ -0,0 +1,16 @@
+echo -e "\e[33m Disabling mysql default verin  \e[0m"
+dnf module disable mysql -y
+
+echo -e "\e[33m Create mysql Repo  \e[0m"
+cp mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo -e "\e[33m Installing mysql \e[0m"
+dnf install mysql-community-server -y
+
+echo -e "\e[33m Enabling and Starting mysql  \e[0m"
+systemctl enable mysqld
+systemctl start mysqld
+
+echo -e "\e[33m Setting up username and password to mysql database \e[0m"
+mysql_secure_installation --set-root-pass ExpenseApp@1
+
